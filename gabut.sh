1a294b7216ba0aee9351491b0a7d16684d8079d8
StianGbt <113441189+StianGbt@users.noreply.github.com>
2022-09-14 17:39:55 +0700

    Add files via upload

---
+++ gabut.sh
@@ -0,0 +1,129 @@
+#!/bin/sh
+blue='\033[34;1m'
+green='\33[0;32m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+rid='\033[31;1m'
+white='\033[34;1m'
+yellow='\033[33;1m'
+
+
+
+clear
+
+toilet -f big -F gay SEPTIAN
+echo
+echo  "SELAMAT DATANG NGNTD!!!"
+echo "®®®®®®®®®®®®®®®®®®®®®"
+echo "® 01.Api Membara    ®"
+echo "®°°°°°°°°°°°°°°°°°°°®"
+echo "® 02.Kereta Api     ®"
+echo "®°°°°°°°°°°°°°°°°°°°®"
+echo "® 03.Hujan Deras    ®"
+echo "®°°°°°°°°°°°°°°°°°°°®"
+echo "® 04.EXIT           ®"
+echo "®®®®®®®®®®®®®®®®®®®®®"
+echo
+#https://www.facebook.com/S.tian17
+echo "©©©©©©©©©©©""©©©©©©©©©©©©©©©©©©©©©"
+echo ">>>>}Pilih Salah Satu Ngntd{<<<<"
+read -p "PILIH YANG BNER KNTL = " OK
+
+if [ $OK = 01 ]
+then
+echo "Tunggu ngtod..."
+sleep 2
+clear
+toilet -f big -F gay GABUT
+echo "Loading..."
+sleep 2
+clear
+echo "HITUNG MUNDUR DIMULAI..."
+sleep 1
+toilet -f big 3
+sleep 2
+toilet -f big 2
+sleep 2
+toilet -f big 1
+sleep 2 
+echo "Loading..."
+sleep 2
+clear
+echo
+cacafire
+
+fi
+#Septian Punya#
+
+if [ $OK = 02 ]
+then
+echo "Tunggu ngtod..."
+sleep 2
+clear
+toilet -f big -F gay GABUT
+echo "Loading..."
+sleep 2
+clear
+echo "HITUNG MUNDUR DIMULAI..."
+sleep 1
+toilet -f big 3
+sleep 2
+toilet -f big 2
+sleep 2
+toilet -f big 1
+sleep 2
+echo "Loading..."
+sleep 2
+clear
+echo
+sl
+
+clear
+sl
+
+clear
+sl
+
+clear
+sl
+
+fi
+#Septian Punya#
+
+
+if [ $OK = 03 ]
+then
+echo "Tunggu ngtod..."
+sleep 2
+clear
+toilet -f big -F gay GABUT
+echo "Loading..."
+sleep 2
+clear
+echo "HITUNG MUNDUR DIMULAI..."
+sleep 1
+toilet -f big 3
+sleep 2
+toilet -f big 2
+sleep 2
+toilet -f big 1
+sleep 2
+echo "Loading..."
+sleep 2
+clear
+echo
+cmatrix
+
+fi
+#Septian Punya#
+
+
+if [ $OK = 04 ]
+then
+echo "Tunggu Ajg..."
+sleep 2
+clear
+toilet -f big -F gay Bye... Asu
+echo
+
+fi
